0c733d6e5d7af02add8ac3069f81d5a94793ff42
olexanderPotebenko <55257818+olexanderPotebenko@users.noreply.github.com>
2019-09-15 13:33:38 +0300

    Add new classes

---
+++ character/behaviorManager.js
@@ -0,0 +1,7 @@
+class BehaviorDamageManager{
+  update(objsArr){
+    let arrWhoHasDamage = arr.filter(item => item.damage == true);
+    let arrWhoHasHealth = arr.filter(item => item.health == true);
+    arr.CharacterDamageController()
+  };
+};
--- scene/scene.js
+++ scene/scene.js
@@ -1,14 +1,22 @@
 class Scene{
-	constructor(cnvArr, characterArr){
-		
+	constructor(mngArr, chrArr){
+		this.mngArr = mngArr;
+		this.chrArr = chrArr;
+		this.eventArr = [];
 	};
 	start(){
-		
+		let mngArr = this.mngArr;
+		for(let i = 0; i < mngArr.length; i++){
+			mngArr[i].start(chrArr, eventArr);
+		};
 	};
 	play(){
-		
+
 	};
 	pause(){
-		
+		let mngArr = this.mngArr;
+		for(let i = 0; i < mngArr.length; i++){
+			mngArr[i].pause();
+		};
 	};
 };
